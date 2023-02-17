5a888805adeace198220e1472aa80ff8b196ebfe
mikepackard-fs <120605913+mikepackard-fs@users.noreply.github.com>
2023-02-16 18:12:44 -0600

    Update dash-server.py

--- dash-server.py
+++ dash-server.py
@@ -1,10 +1,21 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
 from dash import Dash, html, dcc, Input, Output, State
 
-app = Dash(__name__)
+dash_app = Dash(__name__)
 
-app.layout = html.Div([
-  html.H1("Prototype: Dash App in AWS App Runner")
+dash_app.layout = html.Div([
+    html.H1("Prototype: Dash App in AWS App Runner")
 ])
 
 if __name__ == '__main__':
-    app.run_server()
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('dash', '/')
+        config.add_view(dash_app.run_server(), route_name='dash')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
+    
